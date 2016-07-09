ab26142548c2ff382fdb3a7cdeed213666533b26
melaniemagdalene <melanieblazevic@icloud.com>
2016-07-09 12:34:24 -0400

    Fixing git subdirectories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
 	def hello
 		render text: "Hello, World!"
 	end
+
+	def goodbye
+		render text: "Goodbye, World!"
+	end
 	protect_from_forgery with: :exception
 end
